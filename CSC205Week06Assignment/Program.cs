fd4cedafc59a291208a2d69a68c750987eceb7f9
Cameron Hickey <cam@macstir.com>
2022-06-18 10:22:11 -0700

    Current Stage

--- CSC205Week06Assignment/Program.cs
+++ CSC205Week06Assignment/Program.cs
@@ -6,22 +6,29 @@ namespace CSC205Week06
         public static void Main(string[] args)
         {
             Rational r1 = new Rational();
-            r1.numerator = -7;
-            r1.denominator = 1;
-            Rational r2 = new Rational(445,12);
+            r1.numerator = -8;
+            r1.denominator = 3;
+            Rational r2 = new Rational(40,5);
 
-            //r1.WriteRational(r1);
+            //r1.WriteRational(r1); // Testing method
             //r2.WriteRational(r2);
             //Console.WriteLine();
             //r1.Negate(r1);
             //r2.Negate(r2);
             //Console.WriteLine();
-            //r1.Invert(r1);
+            //r1.Invert(r1); // Testing Method
             //r2.Invert(r2);
             //Console.WriteLine();
-            Console.WriteLine(Rational.ToDouble(r1));
-            Console.WriteLine(r2.ToDouble(r2));
-        
+            //Console.WriteLine(Rational.ToDouble(r1)); // Convert the rational to double 
+            //Console.WriteLine(Rational.ToDouble(r2));
+            //Console.WriteLine();
+            //r1.WriteRational(r1); // Confirming the methods have not changed the original Object
+            //r2.WriteRational(r2);
+            //Console.WriteLine();
+            Console.WriteLine( Rational.Reduce(r1)); 
+            Console.WriteLine( Rational.Reduce(r2));
+            Console.WriteLine();
+            Console.WriteLine(Rational.Add(r1, r2)); // In debugger it has the correct value, but it doesn't print it. 
         }
     }
 }
--- CSC205Week06Assignment/Rational.cs
+++ CSC205Week06Assignment/Rational.cs
@@ -8,32 +8,62 @@
 			numerator = 0;
 			denominator = 1;
 		}
-        public Rational(int numerator, int denominator) // Parametric Constructor
-        {
-            this.numerator = numerator;
-            this.denominator = denominator;
-        }
-        public void WriteRational(Rational rational) // Prints rational 
+		public Rational(int numerator, int denominator) // Parametric Constructor
+		{
+			this.numerator = numerator;
+			this.denominator = denominator;
+		}
+		public void WriteRational(Rational rational) // Prints rational 
 		{
 			Console.WriteLine(this.numerator + "/" + this.denominator);
 		}
 		public void Negate(Rational rational) // Reverses sign of the Rational
 		{
 			int neg = this.numerator * -1;
-            Console.WriteLine(neg + "/" + this.denominator);
+			Console.WriteLine(neg + "/" + this.denominator);
 		}
 		public void Invert(Rational rational) // Prints Invert of the rational
 		{
 			Console.WriteLine(this.denominator + "/" + this.numerator);
 		}
-		public static Rational ToDouble(Rational rational)
+		public static double ToDouble(Rational rational) // Convert the Rational to a Double 
 		{
 			Rational ToD = new Rational();
-			double num = Convert.ToDouble(ToD.numerator);
-			double denom = Convert.ToDouble(ToD.denominator);
-			ToD.numerator = (int)num;
-			ToD.denominator = (int)denom;
-			return ToD;
+			double sum = (double)rational.numerator / (double)rational.denominator;
+
+			return sum;
+
+			// ToD.numerator = Convert.ToDouble(rational.numerator);  // Not really sure why it refuses to convert from int to double.
+			// ToD.numerator = (double)rational.numerator; // Requires Cast but Cast is redundant. The fuck is wrong with this programming language?
+		}
+		public static long Reduce(Rational rational)
+		{
+			return GCD(rational.numerator, rational.denominator);
+		}
+		static long GCD(long n1, long n2)
+		{
+			if (n2 == 0)
+			{
+				return n1;
+			}
+			else
+			{
+				return GCD(n2, n1 % n2);
+			}
+		}
+		public static Rational Add(Rational rational1, Rational rational2)
+		{
+			Rational Add = new Rational();
+			int AddNum = rational1.numerator + rational2.numerator;
+			int AddDen = rational1.denominator + rational2.denominator;
+
+			long simptop = AddNum / GCD(AddNum, AddDen);
+			long simpbot = AddDen / GCD(AddNum, AddDen);
+			Add.numerator = (int)simptop;
+			Add.denominator = (int)simpbot;
+			
+			return Add;
 		}
 	}
-}
+}		
+	
